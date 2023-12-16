056f578ca518c04673a098323acdc8afe1456bb4
hatem782 <hatembenechikh100@gmail.com>
2023-12-16 19:16:05 +0100

    fix mongo config

--- node/configs/MongoConfig.js
+++ node/configs/MongoConfig.js
@@ -2,10 +2,13 @@ var mongoose = require("mongoose");
 
 module.exports.ConnectMongo = () => {
   mongoose
-    .connect("mongodb://mongodb:27017/bd", {
+    .connect("mongodb://127.0.0.1:27017/db", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     .then(() => console.log("MongoDB is connected "))
     .catch((err) => console.log(err));
 };
+
+// with docker : mongodb://mongodb:27017/bd
+// without docker : mongodb://127.0.0.1:27017/db
